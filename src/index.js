c280cec765ac7728250f65b68e3268c31932c61d
Thanh Tuan <thanhtuan6582@gmail.com>
2022-03-08 21:17:22 +0700

    update code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min';
+import 'bootstrap/dist/js/popper.min';
+import 'bootstrap/dist/js/jquery-3.2.1.slim.min';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
