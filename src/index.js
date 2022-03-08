e4b23f60fcf97707baccfc52980d1194904eb528
Thanh Tuan <thanhtuan6582@gmail.com>
2022-03-08 22:33:48 +0700

    update push

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import './bootstrap/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
