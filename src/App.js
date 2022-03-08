558551516445bab2b3668357e1f296b6b21e9b9d
Thanh Tuan <thanhtuan6582@gmail.com>
2022-03-08 22:14:23 +0700

    update css

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ function App() {
   return (
     <div className="App">
       <h2>Đồng Hồ Hải Triều</h2>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6">hello </div>
+          <div className="col-lg-6">hello </div>
+          <div className="col-lg-6">hello </div>
+          <div className="col-lg-6">hello </div>
+          <div className="col-lg-6">hello </div>
+          <div className="col-lg-6">hello </div>
+          <div className="col-lg-6">hello </div>
+        </div>
+      </div>
     </div>
   );
 }
