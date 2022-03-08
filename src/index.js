d7ef97e86c4ba55896cff21db642954e1bb70b95
Thanh Tuan <thanhtuan6582@gmail.com>
2022-03-08 21:46:35 +0700

    update boostrap

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import ReactDOM from 'react-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min';
-import 'bootstrap/dist/js/jquery-3.2.1.slim.min';
 
 import './index.css';
 import App from './App';
