6761b10cc1687b77b99cc61c6c0a628f749d72e1
Thanh Tuan <thanhtuan6582@gmail.com>
2022-03-08 22:04:35 +0700

    update 2204

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min'
 
 
