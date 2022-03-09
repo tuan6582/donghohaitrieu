163424d65325281fc00054c3ddd623b73357ce7e
Thanh Tuan <thanhtuan6582@gmail.com>
2022-03-09 18:46:12 +0700

    update 18:46

--- src/Container/Slider/Slider.js
+++ src/Container/Slider/Slider.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Slider.css'
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 function Slider() {
     const settings = {
